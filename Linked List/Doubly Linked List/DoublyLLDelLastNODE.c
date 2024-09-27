75caab297e110220ed093605de5e239b8e114088
Tayeba Joarder -(B210101011) <tayebarizu@gmail.com>
2024-09-27 21:30:10 +0600

    Add files via upload

---
+++ Linked List/Doubly Linked List/DoublyLLDelLastNODE.c
@@ -0,0 +1,71 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node{
+ struct node* prev;
+ int data;
+ struct node* next;
+};
+
+struct node* add_empty(struct node* head, int d){
+  struct node* temp = malloc(sizeof(struct node));
+
+  temp->prev = NULL;
+  temp->data = d;
+  temp->next = NULL;
+
+  head = temp;
+
+  return head;
+};
+
+void add_end(struct node* head, int d){
+  struct node* temp = malloc(sizeof(struct node));
+
+  temp->prev = NULL;
+  temp->data = d;
+  temp->next = NULL;
+
+  struct node* tp = head;
+
+  while(tp->next!=NULL){
+    tp=tp->next;
+  }
+  tp->next = temp;
+  temp->prev = tp;
+}
+
+void delLastNode(struct node* head){
+  struct node* tp = head;
+  struct node* tp2 = NULL;
+  while(tp->next!=NULL){
+    tp=tp->next;
+  }
+  tp2 = tp->prev;
+  tp2->next = NULL;
+  free(tp);
+  tp = NULL;
+}
+
+void print(struct node* head){
+  struct node* ptr = head;
+  while(ptr!=NULL){
+    printf("%d ", ptr->data);
+    ptr=ptr->next;
+  }
+ }
+
+ int main(){
+  struct node* head = NULL;
+  head = add_empty(head,45);
+  add_end(head,34);
+  add_end(head,9);
+
+  printf("Print before deletation: \n");
+  print(head);
+
+   delLastNode(head);
+   printf("\nPrint After deletation: \n");
+   print(head);
+ }
+
